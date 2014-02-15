544e1fb5b160ddfef556b0abb3a4afe75066c026
Cyril "Kyriog" Glapa <cyril.glapa@keuse.fr>
2014-02-15 16:05:18 +0100

    Adding login

--- src/fr/kyriog/android/voxpopuli/GameActivity.java
+++ src/fr/kyriog/android/voxpopuli/GameActivity.java
@@ -2,21 +2,29 @@ package fr.kyriog.android.voxpopuli;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class GameActivity extends Activity {
+	public final static String VP_DATA_USERNAME = "username";
+	public final static String VP_DATA_GAME = "game";
+	public final static String VP_DATA_USER_ID = "userId";
+	public final static String VP_DATA_USER_SESSION = "userSession";
+	
+	private final static int LOGIN_REQUEST_CODE = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivityForResult(intent, LOGIN_REQUEST_CODE);
+		//setContentView(R.layout.activity_game);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.game, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode == Activity.RESULT_OK) {
+			
+		} else
+			finish();
 	}
-
 }
---
+++ src/fr/kyriog/android/voxpopuli/LoginActivity.java
@@ -0,0 +1,54 @@
+package fr.kyriog.android.voxpopuli;
+
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+
+public class LoginActivity extends Activity {
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		WebView web = (WebView) findViewById(R.id.login_webview);
+		WebSettings settings = web.getSettings();
+		settings.setJavaScriptEnabled(true);
+		web.addJavascriptInterface(new JS(), "androidjs");
+		web.setWebViewClient(new Client());
+		web.loadUrl("http://vox-populi.richie.fr/login");
+	}
+	
+	private class Client extends WebViewClient {
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			if("http://vox-populi.richie.fr/".equals(url))
+				view.loadUrl("javascript:window.androidjs.getHtml(document.getElementsByTagName('html')[0].innerHTML);");
+		}
+	}
+	
+	private class JS {
+		@JavascriptInterface
+		public void getHtml(String rawHtml) {
+			String[] html = rawHtml.split("\n");
+			String username = html[2].split("@", 2)[1].split("\\.", 2)[0];
+			String game = html[5].split("/game/", 2)[1].split("\"", 2)[0];
+			String userId = html[12].split("user_id=", 2)[1].split("&", 2)[0];
+			String userSession = html[12].split("user_session=", 2)[1].split("&", 2)[0];
+			
+			Intent data = new Intent();
+			data.putExtra(GameActivity.VP_DATA_USERNAME, username);
+			data.putExtra(GameActivity.VP_DATA_GAME, game);
+			data.putExtra(GameActivity.VP_DATA_USER_ID, userId);
+			data.putExtra(GameActivity.VP_DATA_USER_SESSION, userSession);
+			
+			setResult(Activity.RESULT_OK, data);
+			finish();
+		}
+	}
+}
